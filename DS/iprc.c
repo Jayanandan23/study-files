00bb34320ec84eb3f981e3c64955a98fd5506924
Jayanandan23 <jayachandrankn9@gmail.com>
2025-05-12 08:54:02 +0530

    Update iprc.c

--- DS/iprc.c
+++ DS/iprc.c
@@ -1,161 +1,126 @@
 #include <stdio.h>
-#include <string.h>
-#include <limits.h>
-#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>  // Needed for strlen
 #define MAX 100
-int top=-1;
-char stack[MAX];
-int isfull()
+
+typedef struct
 {
-        return top==MAX-1;
+    char items[MAX];
+    int top;
+} stack;
+
+void initStack(stack *s)
+{
+    s->top = -1;
 }
-int isEmpty()
+
+void push(stack *s, char c)
 {
-        return top==-1;
+    s->items[++s->top] = c;
 }
-void push(char item)
+
+char pop(stack *s)
 {
-        if(isfull())
+    return s->items[s->top--];
+}
+
+char peek(stack *s)
+{
+    return s->items[s->top];
+}
+
+int precedence(char op)
+{
+    switch (op)
+    {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        case '^':
+            return 3;
+        default:
+            return 0;
+    }
+}
+
+void reverse(char *str)
+{
+    int i, len = strlen(str);
+    for (i = 0; i < len / 2; i++)
+    {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+}
+
+void infixtoprefix(char *infix, char *prefix)
+{
+    stack s;
+    initStack(&s);
+    int i, j = 0;
+
+    reverse(infix);
+
+    for (i = 0; infix[i] != '\0'; i++)
+    {
+        if (infix[i] == '(')
+            infix[i] = ')';
+        else if (infix[i] == ')')
+            infix[i] = '(';
+    }
+
+    for (i = 0; infix[i] != '\0'; i++)
+    {
+        if (isalnum(infix[i]))
         {
-                return 0;
+            prefix[j++] = infix[i];
         }
-        top++;
-        stack[top]=item;
-}
-int pop()
-{
-        if(isEmpty())
+        else if (infix[i] == '(')
         {
-                return INT_MIN;
-	}
-	return stack[top--];
-}
-int peek()
-{
-	if(isEmpty())
-	{
-		return INT_MIN;
-	}
-	return stack[top];
-}
-int checkifoperand(char ch)
-{
-	return (ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z');
-}
-int precedence(char ch)
-{
-	switch(ch)
-	{
-		case'+':
-		case'-':
-			return 1;
-		case'*':
-                case'/':
-                        return 2;
-		case'^':
-			return 3;
-	}
-	return -1;
-}
-int getpostfix(char*expression)
-{
-	int i,j;
-	for(i=0;j=-1;expression[i];i++)
-	{
-		if(checkifoperand(expression[i]))
-		{
-			expression[++j]=expression[i];
-		}
-		else if(expression[i])=='(')
-		{
-			push(expression[i]);
-		}
-		else if(expresssion[i]==')')
-		{
-			while(isEmpty(stack)&&peek(stack)!='(')
-			{
-				expression[++j]=pop(stack);
-			}
-			if(!isEmpty(stack)&&peek(stack)!='(')
-			{
-				return -1;
-			}
-			else
-			{
-				pop(stack);
-			}
-		}
-		else
-		{
-			while(!isEmpty(stack)&&precedence(expression[i]<=precedence(peek(stack)))
-			{
-				expression[++j]=pop(stack);
-			}
-			push(expression[i]);
-		}
-	}
-	while(!isEmpty(stack))
-	{
-		expression[++j]=pop(stack);
-	}
-	expression[++j]='\0';
-}
-void reverse(char*exp)
-{
-	int size=strlen=strlen(exp);
-	int j=size,i=0;
-	char temp[j--]='\0';
-	while(exp[i]!='\0')
-	{
-		temp[j]=exp[j];
-		j--;
-		i++;
-	}
-	strcpy(exp,temp)
-}
-void brackets(char*exp)
-{
-	int i=0;
-	while(exp[i]!='\0')
-	{
-		if(exp[i]=='(')
-		{
-			exp[i]=='(');
-		}
-		else if(exp[i]==')')
-		{
-			exp[i]='(';
-		}
-		i++;
-	}
-}
-voidinfixtoprefix(char*exp)
-{
-	int size=strlen(exp);
-	reverse(exp);
-	brackets(exp);
-	reverse(exp);
+            push(&s, infix[i]);
+        }
+        else if (infix[i] == ')')
+        {
+            while (peek(&s) != '(')
+            {
+                prefix[j++] = pop(&s);
+            }
+            pop(&s);
+        }
+        else
+        {
+            while (s.top != -1 && precedence(peek(&s)) > precedence(infix[i]))
+            {
+                prefix[j++] = pop(&s);
+            }
+            push(&s, infix[i]);
+        }
+    }
+
+    while (s.top != -1)
+    {
+        prefix[j++] = pop(&s);
+    }
+
+    prefix[j] = '\0';
+    reverse(prefix);
 }
 
 int main()
 {
-	char expression[100];
-	printf("The infix is :");
-	scanf("%s",expression);
-	printf("%s\n",expression);
-	infixtoprefix(expression);
-	printf("The prefix is :");
-	printf("%s\n",expression);
-	return 0;
+    char infix[MAX], prefix[MAX];
+    printf("Enter the Infix Expression: ");
+    scanf("%s", infix);
+    infixtoprefix(infix, prefix);
+    printf("Prefix Expression: %s\n", prefix);
+    return 0;
 }
 
-
-
-
-
-
-
-
+		
 
 
 
